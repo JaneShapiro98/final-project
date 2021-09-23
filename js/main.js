e8477081675921bb2fefac7be4507ca2ba0b2598
JaneShapiro98 <75447045+JaneShapiro98@users.noreply.github.com>
2021-09-23 08:52:47 -0400

    thursday-am

--- js/main.js
+++ js/main.js
@@ -1,18 +1,12 @@
-
-const hamburgerDisplay = document.getElementById('hamburger');
-
-const hamburgerDisplayClose = document.getElementById('menu-close');
-
-const hamburgerMenu = document.getElementById('menu');
+const hamburgerDisplay = document.getElementsByClassName("hamburger");
 
 hamburgerDisplay.addEventListener('click', openHamburger);
 
-hamburgerDisplayClose.addEventListener("click", closeHamburger);
-
 function openHamburger() {
-  hamburgerMenu.classList.add('show-menu');
-}
-
-function  closeHamburger() {
-  hamburgerMenu.classList.remove('show-menu');
+  var x = document.getElementsByClassName("menu");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
 }
--- main.js
+++ main.js
@@ -1,7 +1,14 @@
+// function myFunction() {
+//   var x = document.getElementById("myLinks");
+//   if (x.style.display === "block") {
+//     x.style.display = "none";
+//   } else {
+//     x.style.display = "block";
+//   }
+// }
+
 
 const hamburgerDisplay = document.getElementById('hamburger');
-
-const hamburgerDisplayClose = document.getElementById('menu-close');
 
 const hamburgerMenu = document.getElementById('menu');
 
@@ -17,32 +24,32 @@ function  closeHamburger() {
   hamburgerMenu.classList.remove('show-menu');
 }
 
-function validateForm() {
-  var name =  document.getElementById('name').value;
-  if (name == "") {
-      document.querySelector('.status').innerHTML = "Name cannot be empty";
-      return false;
-  }
-  var email =  document.getElementById('email').value;
-  if (email == "") {
-      document.querySelector('.status').innerHTML = "Email cannot be empty";
-      return false;
-  } else {
-      var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-      if(!re.test(email)){
-          document.querySelector('.status').innerHTML = "Email format invalid";
-          return false;
-      }
-  }
-  var subject =  document.getElementById('subject').value;
-  if (subject == "") {
-      document.querySelector('.status').innerHTML = "Subject cannot be empty";
-      return false;
-  }
-  var message =  document.getElementById('message').value;
-  if (message == "") {
-      document.querySelector('.status').innerHTML = "Message cannot be empty";
-      return false;
-  }
-  document.querySelector('.status').innerHTML = "Sending...";
-}
+// function validateForm() {
+//   var name =  document.getElementById('name').value;
+//   if (name == "") {
+//       document.querySelector('.status').innerHTML = "Name cannot be empty";
+//       return false;
+//   }
+//   var email =  document.getElementById('email').value;
+//   if (email == "") {
+//       document.querySelector('.status').innerHTML = "Email cannot be empty";
+//       return false;
+//   } else {
+//       var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+//       if(!re.test(email)){
+//           document.querySelector('.status').innerHTML = "Email format invalid";
+//           return false;
+//       }
+//   }
+//   var subject =  document.getElementById('subject').value;
+//   if (subject == "") {
+//       document.querySelector('.status').innerHTML = "Subject cannot be empty";
+//       return false;
+//   }
+//   var message =  document.getElementById('message').value;
+//   if (message == "") {
+//       document.querySelector('.status').innerHTML = "Message cannot be empty";
+//       return false;
+//   }
+//   document.querySelector('.status').innerHTML = "Sending...";
+// }
